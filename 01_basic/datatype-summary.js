61500f6e5210448003449951ebcdb545d26e0ee5
Tanmay Rane <135257857+tanmayR18@users.noreply.github.com>
2024-02-05 16:13:06 +0000

    heap-stack explained

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,11 @@
+// Stack => Primitive  |||  Heap => Non-primitve
+
+/*
+
+all the primitive data types are stored in the stack 
+and all the non primitive are stored in the heap 
+
+when we create a copy of premitive the value is passed
+and when we create a copy of non primitive the reference  or the address of the value from the heap memory is passed
+
+*/
