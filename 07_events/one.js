3ab8b9144f8e08a18d1e8a4f7f968156a5aeeb63
Tanmay Rane <135257857+tanmayR18@users.noreply.github.com>
2024-03-08 02:20:26 +0000

    events completed

---
+++ 07_events/one.js
@@ -0,0 +1,11 @@
+// during the bubbling face if you don't what that the outer element's listenster should not be propopaget
+// use event.stopPropogration()
+
+// document.getElementById('images').addEventListener('click', function(e) {
+//     console.log("clicked inside the ul");
+// }, false);
+
+// document.getElementById('owl').addEventListener('click', function(e) {
+//     console.log("owl clicked");
+//     e.stopPropagation();
+// }, false);
